4ef2b8d5991f560477bf60869081d34e157a3fe9
alglebov <alglebov@ozon.ru>
2022-05-29 18:44:30 +0300

    working grpc + cleanup

---
+++ InterestingAlgorithms/Grpc/AlgorithmsGrpcService.cs
@@ -0,0 +1,27 @@
+﻿using System.Threading.Tasks;
+using AlgorithmsGrpc;
+using Grpc.Core;
+using InterestingAlgorithms.BLL.Services.Intarface;
+
+namespace AlgorithmsGrpc
+{
+    public class AlgorithmsService : Algorithms.AlgorithmsBase
+    {
+        private readonly ICalculatorService _calculatorServices;
+
+        public AlgorithmsService(ICalculatorService calculatorServices)
+        {
+            _calculatorServices = calculatorServices;
+        }
+
+        public override async Task<GetResultAdditionsResponse> GetResultAdditions(GetResultAdditionsRequest request, ServerCallContext context)
+        {
+            var result = await _calculatorServices.GetResultAdditions(request.First, request.Second, token: context.CancellationToken);
+
+            return new GetResultAdditionsResponse
+            {
+                Result = result
+            };
+        }
+    }
+}
--- InterestingAlgorithms/Startup.cs
+++ InterestingAlgorithms/Startup.cs
@@ -1,18 +1,13 @@
+using System.Threading.Tasks;
+using AlgorithmsGrpc;
 using InterestingAlgorithms.BLL.Services;
 using InterestingAlgorithms.BLL.Services.Intarface;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace InterestingAlgorithms
 {
@@ -28,7 +23,7 @@ namespace InterestingAlgorithms
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
+            services.AddGrpc();
             services.AddControllers();
             services.AddScoped<ICalculatorService, CalculatorService>();
             services.AddSwaggerGen(c =>
@@ -47,8 +42,6 @@ namespace InterestingAlgorithms
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "InterestingAlgorithms v1"));
             }
 
-            app.UseHttpsRedirection();
-
             app.UseRouting();
 
             app.UseAuthorization();
@@ -56,6 +49,7 @@ namespace InterestingAlgorithms
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+                endpoints.MapGrpcService<AlgorithmsService>();
             });
         }
     }
